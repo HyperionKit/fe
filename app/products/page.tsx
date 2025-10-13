1cc8ba723edca2c3197c18a9063b2e1e7ddb168a
JustineDevs <pcg0255@gmail.com>
2025-10-13 10:07:17 +0800

    feat: add products page route with interactive components integration

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,7 +5,7 @@ import ProductsOverviewPage from "@/components/products-overview-page";
 export default function Home() {
   return (
     <div className="w-full">
-      <div className="pt-16 sm:pt-18 lg:pt-20">
+      <div className="pt-16 sm:pt-20 lg:pt-24">
         <ProductsHeroPage/>
         <ProductsOverviewPage/>
       </div>
