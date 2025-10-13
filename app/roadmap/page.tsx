b99151b5eaf4356e993a07aa33fbd6b03c7a54c7
JustineDevs <pcg0255@gmail.com>
2025-10-13 10:07:21 +0800

    feat: add roadmap page route with Dither background and timeline integration

--- app/roadmap/page.tsx
+++ app/roadmap/page.tsx
@@ -5,7 +5,7 @@ import RoadmapTimeline from '@/components/roadmap-timeline'
 const page = () => {
   return (
     <div className="w-full">
-      <div className="pt-16 sm:pt-18 lg:pt-20">
+      <div className="pt-16 sm:pt-20 lg:pt-24">
         <RoadmapHeroPage />
         <RoadmapTimeline />
       </div>
