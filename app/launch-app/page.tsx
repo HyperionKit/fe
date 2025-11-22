580ed73432a16265cfc2d09b323bebbec894f36a
JustineDevs <TraderGOfficial@gmail.com>
2025-11-22 18:23:01 +0800

    chore: Updated page.tsx

--- app/launch-app/page.tsx
+++ app/launch-app/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { ErrorBoundary } from '@/components/error-boundary'
-import LaunchAppHeroPage from '@/components/launch-app-hero-page'
-import LaunchRecentApp from '@/components/launch-app-recent-app'
-import LaunchAppCommunity from '@/components/launch-app-community'
+import { ErrorBoundary } from '@/components/utils/error-boundary'
+import LaunchAppHeroPage from '@/components/pages/launch-app-hero-page'
+import LaunchRecentApp from '@/components/pages/launch-app-recent-app'
+import LaunchAppCommunity from '@/components/pages/launch-app-community'
 
 const page = () => {
   return (
