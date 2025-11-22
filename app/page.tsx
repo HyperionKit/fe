cbc67a43367bbf1cc4c6fcb9fe3851bf1e3ad05c
JustineDevs <TraderGOfficial@gmail.com>
2025-11-22 18:23:01 +0800

    chore: Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import HeroPage from '@/components/hero-page';
-import TimelinePage from '@/components/timeline-page';
-import CardPage from '@/components/cards-page';
-import RewardPage from '@/components/rewards-page';
+import HeroPage from '@/components/pages/hero-page';
+import TimelinePage from '@/components/pages/timeline-page';
+import CardPage from '@/components/pages/cards-page';
+import RewardPage from '@/components/pages/rewards-page';
 
 export default function Home() {
   return (
